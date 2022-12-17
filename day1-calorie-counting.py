ea0420eb93ade3323e16514783f6b9b8899d0016
furnivall <danielfurnivall@gmail.com>
2022-12-17 19:57:25 +0000

    took a line out

--- day1-calorie-counting.py
+++ day1-calorie-counting.py
@@ -1,5 +1,4 @@
-with open ('day1-1-input.txt') as f:
-    elves = f.read().split('\n\n')
+with open ('day1-1-input.txt') as f: elves = f.read().split('\n\n')
 print(max([sum(map(int,elf.split('\n'))) for elf in elves[:-1]]))
 
 
