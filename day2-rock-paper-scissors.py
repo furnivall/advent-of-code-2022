08775c376a5ec76da97d57f826a6e54d2b2eefca
furnivall <danielfurnivall@gmail.com>
2022-12-28 18:44:41 +0000

    day 2 part 1

--- day2-rock-paper-scissors.py
+++ day2-rock-paper-scissors.py
@@ -1,9 +1,8 @@
-with open("day2-1-input.txt") as f:
-    # map to subtract 64 from ascii vals to get actual scores for each of our moves. 
-    data = map(lambda i: (ord(i[0])-64, ord(i[2])-87), f.readlines())
-
 def part1():
-    return sum([(3 if (y - x) == 0 else 0 if (y - x) in (-1, 2) else 6) + y for x, y in data])
+    with open("day2-1-input.txt") as f:
+        # map to subtract 64 from ascii vals to get actual scores for each of our moves. 
+        data = map(lambda i: (ord(i[0])-64, ord(i[2])-87), f.readlines())
+        return sum([(3 if (y - x) == 0 else 0 if (y - x) in (-1, 2) else 6) + y for x, y in data])
 
 print(part1())
 
