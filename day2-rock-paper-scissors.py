55a966428fb4b8b8aa224527b743c0aa92dacbd2
furnivall <danielfurnivall@gmail.com>
2022-12-29 01:16:40 +0000

    day 2 part 2 a bit cleaned up

--- day2-rock-paper-scissors.py
+++ day2-rock-paper-scissors.py
@@ -6,8 +6,9 @@ def part1():
 
 def part2():
     with open("day2-1-input.txt") as f:
+        # reduced value by 1 for opponent's ascii so as to utilise zero indexing
         data = list(map(lambda i: (ord(i[0])-65, ord(i[2])-87), f.readlines()))
-        return sum([i[0] + 4 if i[1] == 2 else [3,1,2][i[0]] if i[1] == 1 else [8, 9, 7][i[0]] for i in data])
+        return sum([x + 4 if y == 2 else [3,1,2][x] if y == 1 else [8, 9, 7][x] for x, y in data])
 
 
 print(part1())
